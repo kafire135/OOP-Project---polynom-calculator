166d1c9d8b47dd077610b848711ff0ebcf9658d8
kafire135 <kfirniss@post.bgu.ac.il>
2023-05-07 11:23:19 +0300

    main

--- src/PolynomialCalculator/Main.java
+++ src/PolynomialCalculator/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
 
     public static void main (String [] args){
-        Polynomial p1= Polynomial.build("1 3 0 5 1 9 9 8");
+        Polynomial p1= Polynomial.build("1 3 0 5 1 9 9 9");
         Polynomial p2= Polynomial.build("2 7 0 6 0 0 9 7");
         Polynomial p3= Polynomial.build("-1/5 3 0 0 -21 20/8");
         Polynomial ans = p1.add(p2);
